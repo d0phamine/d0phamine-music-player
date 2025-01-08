2fc0a854d765a36ac466aeb8f287beba39643524
d0phamin3 <sizow123@gmail.com>
2025-01-08 11:03:59 +0300

    fix icons paths

--- public/utils/config.js
+++ public/utils/config.js
@@ -1,17 +1,21 @@
-const { join, resolve } = require("path");
-const isDev = require("electron-is-dev");
+const { resolve } = require("path")
+const os = require("os")
+const isDev = require("electron-is-dev")
 
-const iconPath = resolve(__dirname, "..", "icon_16x16.png");
+const iconPath =
+	os.platform() === "darwin"
+		? resolve(__dirname, "../icons", "icon-mono_16x16@2x.png")
+		: resolve(__dirname, "../icons", "icon_16x16@2x.png")
 
 let config = {
-    appName: "d0phamine music player",
-    icon: iconPath,
-    tray: null,
-    isQuiting: false,
-    mainWindow: null,
-    popupWindow: null,
-    isDev,
-};
+	appName: "d0phamine music player",
+	icon: iconPath,
+	tray: null,
+	isQuiting: false,
+	mainWindow: null,
+	popupWindow: null,
+	isDev,
+}
 
-module.exports = config;
+module.exports = config
 
